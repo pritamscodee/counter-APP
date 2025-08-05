a63da8ea4b98fa9429390985d51b34296d0044f8
pritamscodee <codewithpritam29@gmail.com>
2025-08-05 18:43:38 +0000

    adding autoplay button which play automatically count ing and stop when click on stop

--- bb.js
+++ bb.js
@@ -4,6 +4,7 @@ let countbtn=document.getElementById("btn-countbtn")
 let displaycount = document.getElementsByClassName("count-display")[0]
 let count =0;
 let btn123 = document.getElementById("random");
+let smartbtn = document.getElementById("autobtn") ;
 
 function display(){
 
@@ -42,3 +43,37 @@ btn123.addEventListener('click',function (){
     display()
 
 })
+
+
+let  intervalid;
+
+smartbtn.addEventListener("click",  autoplaying);
+
+
+function autoplaying(){
+    
+
+if(intervalid==null){
+   intervalid =   setInterval(()=>{
+        count++
+        display();
+    },1000)
+ 
+   smartbtn.textContent="stop"
+
+
+}else{
+
+   
+    clearInterval(intervalid);
+    intervalid= null;
+    count = 0;
+    display();
+    smartbtn.textContent="Auto +1"
+}
+
+}
+
+
+
+
